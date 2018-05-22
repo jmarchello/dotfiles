de2025d1ec10ad25e035c27888942bc01581cc90
Josh Marchello <josh@jmarchello.com>
2018-05-22 17:39:54 -0500

    adding full directories to my dotfiles

--- link.sh
+++ link.sh
@@ -1,10 +1,10 @@
 # VIM
-ln -sfiv ~/dotfiles/vim/.vimrc ~
+ln -sfhiv ~/dotfiles/vim/.vimrc ~
+ln -sfhiv ~/dotfiles/vim/.vim ~
 
 # Tmux
 # ln -sfiv ~/dotfiles/tmux/.tmux.conf ~
 
 # ZSH
-ln -sfiv ~/dotfiles/zsh/.zshrc ~
-ln -sfiv ~/dotfiles/zsh/custom/josh.zsh ~/.oh-my-zsh/custom
-ln -sfiv ~/dotfiles/zsh/custom/aliases.zsh ~/.oh-my-zsh/custom
+ln -sfhiv ~/dotfiles/zsh/.zshrc ~
+ln -sfhiv ~/dotfiles/zsh/custom/* ~/.oh-my-zsh/custom
