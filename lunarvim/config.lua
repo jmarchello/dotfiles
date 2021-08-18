d53c24598b03c44ce97815132f9a1afce5354a3f
Josh Marchello <josh.marchello@cybergrx.com>
2021-08-18 15:40:38 -0500

    updated lunarvim config

--- lunarvim/config.lua
+++ lunarvim/config.lua
@@ -28,10 +28,12 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.nvimtree.side = "right"
+lvim.builtin.nvimtree.show_icons.git = 1
 lvim.builtin.nvimtree.quit_on_open = 1
+lvim.builtin.nvimtree.auto_open = 0
 lvim.builtin.nvimtree.hide_dotfiles = 0
+vim.g.rooter_patterns = {'.git', '_darcs', '.hg', '.bzr', '.svn', 'Makefile', 'package.json', 'requirements.txt', 'Gemfile', 'go.mod'}
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {}
