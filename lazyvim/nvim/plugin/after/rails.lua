aa0836f51115839121069724d03e841c5e876c14
Josh Marchello <josh@jmarchello.com>
2024-12-20 10:34:56 -0600

    ruby test toggle handles libraries now

--- lazyvim/nvim/plugin/after/rails.lua
+++ lazyvim/nvim/plugin/after/rails.lua
@@ -1,13 +1,36 @@
 local wk = require("which-key")
 
-function _toggle_test_file()
-  local buffer_path = vim.api.nvim_buf_get_name(0)
-  if string.match(buffer_path, "test/") then
-    buffer_path = string.gsub(buffer_path, "test/", "app/")
-    buffer_path = string.gsub(buffer_path, "_test.rb", ".rb")
+local function _get_source_dir()
+  local cwd = vim.fn.getcwd()
+  local app_path = cwd .. "/app"
+  if vim.fn.isdirectory(app_path) == 1 then
+    return "app/"
   else
-    buffer_path = string.gsub(buffer_path, "app/", "test/")
-    buffer_path = string.gsub(buffer_path, ".rb", "_test.rb")
+    return "lib/"
+  end
+end
+
+local function _get_test_dir()
+  local cwd = vim.fn.getcwd()
+  local app_path = cwd .. "/spec"
+  if vim.fn.isdirectory(app_path) == 1 then
+    return "spec/"
+  else
+    return "test/"
+  end
+end
+
+function _toggle_test_file()
+  local source_dir = _get_source_dir()
+  local test_dir = _get_test_dir()
+  local test_file_suffix = "_" .. test_dir:gsub("/", ".rb")
+  local buffer_path = vim.api.nvim_buf_get_name(0)
+  if string.match(buffer_path, test_dir) then
+    buffer_path = string.gsub(buffer_path, test_dir, source_dir)
+    buffer_path = string.gsub(buffer_path, test_file_suffix, ".rb")
+  else
+    buffer_path = string.gsub(buffer_path, source_dir, test_dir)
+    buffer_path = string.gsub(buffer_path, ".rb", test_file_suffix)
   end
   if not vim.fn.filereadable(buffer_path) then
     if vim.fn.input("Create test file? [y/n]: ") == "n" then
